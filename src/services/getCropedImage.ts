99b944782329bd20cb5aa62719e32fc5cb46d3d7
AhmadKhan9 <mafriand77@gmail.com>
2023-09-30 22:57:57 +0500

    Handle games with no image

--- src/services/getCropedImage.ts
+++ src/services/getCropedImage.ts
@@ -1,5 +1,6 @@
+import image from '../assets/no-image-placeholder-6f3882e0.webp'
 const getCropedImage = (url : string)=> {
-    if(!url) return '';
+    if(!url) return image;
     const req = 'media/'
     const target = url.indexOf(req) + req.length
     return url.slice(0,target) + 'crop/600/400/' + url.slice(target)
